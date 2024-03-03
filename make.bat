7bb8a3c8b629270865a96635003e08ced4d45dea
xrstf <git@xrstf.de>
2024-03-03 17:47:13 +0100

    add matroska chapter format

--- make.bat
+++ make.bat
@@ -2,8 +2,12 @@
 
 cd losslesscut
 llc-chapter-converter -format=vorbis .
+llc-chapter-converter -format=matroska .
 
 rem mv *.txt vorbis
 for %%x in (*.txt) do move "%%x" ..\vorbis\
+
+rem mv *.xml matroska
+for %%x in (*.xml) do move "%%x" ..\matroska\
 
 cd ..
