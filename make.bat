2cf82dd5996ca3719213b100ece56fda875f75ee
xrstf <git@xrstf.de>
2024-03-03 17:57:56 +0100

    re-organize

--- make.bat
+++ make.bat
@@ -5,9 +5,18 @@ llc-chapter-converter -format=vorbis .
 llc-chapter-converter -format=matroska .
 
 rem mv *.txt vorbis
-for %%x in (*.txt) do move "%%x" ..\vorbis\
+for %%x in (*.txt) do move "%%x" ..\chapters\vorbis\
 
 rem mv *.xml matroska
-for %%x in (*.xml) do move "%%x" ..\matroska\
+for %%x in (*.xml) do move "%%x" ..\chapters\matroska\
+
+rem cp *.llc edl
+for %%x in (*.llc) do copy "%%x" ..\edl\
+
+cd ..
+
+cd edl
+llc-to-edl .
+for %%x in (*.llc) do del "%%x"
 
 cd ..
