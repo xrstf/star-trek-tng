911bf0252d4b6cf89a9cf38e23e1e05901eb6eb0
xrstf <git@xrstf.de>
2024-03-03 16:19:19 +0100

    move files

--- make.bat
+++ make.bat
@@ -1,6 +1,9 @@
 @echo off
 
+cd losslesscut
 llc-chapter-converter -format=vorbis .
 
 rem mv *.txt vorbis
-for %%x in (*.txt) do move "%%x" vorbis
+for %%x in (*.txt) do move "%%x" ..\vorbis\
+
+cd ..
